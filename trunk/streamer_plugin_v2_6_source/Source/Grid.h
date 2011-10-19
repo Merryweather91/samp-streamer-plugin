ba32328385240fbccb5ec111536b757b5c0e10da
insert.something.of.use.here@gmail.com <insert.something.of.use.here@gmail.com@034ff0a7-5c99-be96-f678-ba8f989ebc60>
2011-10-19 22:10:09 +0000

    Add v2.6

---
+++ trunk/streamer_plugin_v2_6_source/Source/Grid.h
@@ -0,0 +1,60 @@
+/*
+    SA-MP Streamer Plugin v2.6
+    Copyright © 2011 Incognito
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef GRID_H
+#define GRID_H
+
+class Grid
+{
+public:
+	Grid();
+
+	void addArea(const Element::SharedArea &area);
+	void addCheckpoint(const Element::SharedCheckpoint &checkpoint);
+	void addMapIcon(const Element::SharedMapIcon &mapIcon);
+	void addObject(const Element::SharedObject &object);
+	void addPickup(const Element::SharedPickup &pickup);
+	void addRaceCheckpoint(const Element::SharedRaceCheckpoint &raceCheckpoint);
+	void addTextLabel(const Element::SharedTextLabel &textLabel);
+	void eraseAllItems(int type);
+	void findNearbyCells(Player &player, std::vector<SharedCell> &playerCells);
+	void rebuildGrid();
+	void removeArea(const Element::SharedArea &area, bool reassign = false);
+	void removeCheckpoint(const Element::SharedCheckpoint &checkpoint, bool reassign = false);
+	void removeMapIcon(const Element::SharedMapIcon &mapIcon, bool reassign = false);
+	void removeObject(const Element::SharedObject &object, bool reassign = false);
+	void removePickup(const Element::SharedPickup &pickup, bool reassign = false);
+	void removeRaceCheckpoint(const Element::SharedRaceCheckpoint &raceCheckpoint, bool reassign = false);
+	void removeTextLabel(const Element::SharedTextLabel &textLabel, bool reassign = false);
+
+	float cellDistance;
+	float cellSize;
+private:
+	SharedCell globalCell;
+
+	boost::unordered_map<std::pair<int, int>, SharedCell> cells;
+
+	Eigen::Matrix<float, 2, 9> translationMatrix;
+
+	void calculateTranslationMatrix();
+	void cleanVisibleCell(SharedCell &visibleCell, const boost::unordered_set<std::pair<int, int> > &discoveredCells);
+	void eraseCellIfEmpty(const SharedCell &cell);
+	std::pair<int, int> getCellID(const Eigen::Vector2f &position, bool insert = true);
+};
+
+#endif
---
+++ trunk/streamer_plugin_v2_6_source/Source/Manipulation.h
@@ -0,0 +1,68 @@
+/*
+    SA-MP Streamer Plugin v2.6
+    Copyright © 2011 Incognito
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+namespace Manipulation
+{
+	enum
+	{
+		AttachedPlayer,
+		AttachedVehicle,
+		Color,
+		DrawDistance,
+		ExtraID,
+		InteriorID,
+		MaxX,
+		MaxY,
+		MaxZ,
+		MinX,
+		MinY,
+		MinZ,
+		ModelID,
+		MoveRX,
+		MoveRY,
+		MoveRZ,
+		MoveSpeed,
+		MoveX,
+		MoveY,
+		MoveZ,
+		NextX,
+		NextY,
+		NextZ,
+		PlayerID,
+		RX,
+		RY,
+		RZ,
+		Size,
+		StreamDistance,
+		Style,
+		TestLOS,
+		Type,
+		WorldID,
+		X,
+		Y,
+		Z
+	};
+
+	int getFloatData(AMX *amx, cell *params);
+	int setFloatData(AMX *amx, cell *params);
+	int getIntData(AMX *amx, cell *params);
+	int setIntData(AMX *amx, cell *params);
+	int isInArrayData(AMX *amx, cell *params);
+	int appendArrayData(AMX *amx, cell *params);
+	int removeArrayData(AMX *amx, cell *params);
+}
