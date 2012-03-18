836990bad80333733bb9b19e9d53a9e3e5f1b41d
insert.something.of.use.here@gmail.com <insert.something.of.use.here@gmail.com@034ff0a7-5c99-be96-f678-ba8f989ebc60>
2012-03-18 17:08:44 +0000

    Fix Streamer_GetArrayData overflow

--- trunk/src/Utility.h
+++ trunk/src/Utility.h
@@ -1,5 +1,5 @@
 /*
-    SA-MP Streamer Plugin v2.6
+    SA-MP Streamer Plugin v2.6.1
     Copyright © 2012 Incognito
 
     This program is free software: you can redistribute it and/or modify
@@ -169,9 +169,9 @@ namespace Utility
 		cell *array = NULL;
 		std::size_t i = 0;
 		amx_GetAddr(amx, input, &array);
-		for (std::size_t c = 0; c < static_cast<std::size_t>(size); ++c)
+		for (std::size_t c = 0; c < N; ++c)
 		{
-			if (c >= N)
+			if (i == static_cast<std::size_t>(size))
 			{
 				return false;
 			}
